220cda64e1a49bba19f4c92be3b76ef666f54d84
Yuxiang Huang <yhuang30@stuy.edu>
2022-08-27 20:07:10 -0400

    Chained three scenes done

--- Astro Party/Assets/Scripts/ScreenManager.cs
+++ Astro Party/Assets/Scripts/ScreenManager.cs
@@ -6,11 +6,12 @@ public class ScreenManager : MonoBehaviour
 {
     public GameObject startScreen;
     public GameObject shipScreen;
+    public GameObject lastScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startScreen.SetActive(true);
     }
 
     // Update is called once per frame
@@ -19,9 +20,27 @@ public class ScreenManager : MonoBehaviour
         
     }
 
-    public void start ()
+    public void nextToShip ()
     {
         startScreen.SetActive(false);
+        shipScreen.SetActive(true);
+    }
 
+    public void nextToLast()
+    {
+        shipScreen.SetActive(false);
+        lastScreen.SetActive(true);
+    }
+
+    public void backToStart()
+    {
+        shipScreen.SetActive(false);
+        startScreen.SetActive(true);
+    }
+
+    public void backToShip()
+    {
+        lastScreen.SetActive(false);
+        shipScreen.SetActive(true);
     }
 }
