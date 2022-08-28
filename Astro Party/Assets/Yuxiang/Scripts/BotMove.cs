4791a93c555e3f03612770d043193dd64226c5cd
Yuxiang Huang <yhuang30@stuy.edu>
2022-08-27 21:53:53 -0400

    Need to scale the map done.

--- Astro Party/Assets/Yuxiang/Scripts/BotMove.cs
+++ Astro Party/Assets/Yuxiang/Scripts/BotMove.cs
@@ -43,59 +43,60 @@ public class BotMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //agent.SetDestination(transform.position + new Vector3 (100, 0, 0);
         agent.SetDestination(player.transform.position);
         Debug.Log(player.transform.position);
-
-        if (reloadTime == 0) {
-
-            reloadTime = 2;
-
-            float angle = transform.rotation.ToEulerAngles().y;
-
-            if (shootMode == "laser")
-            {
-                //5000 is half the length of laserbeam
-                Instantiate(laser, transform.position +
-                new Vector3((bulletDis + 5000) * Mathf.Sin(angle), 0, (bulletDis + 5000) * Mathf.Cos(angle)),
-                transform.rotation);
-
-                playerAudio.PlayOneShot(laserSound);
-
-                playerRb.constraints = RigidbodyConstraints.FreezePosition;
-
-                StartCoroutine("ableToMove");
-
-                shootMode = "normal";
-            }
-
-            if (shootMode == "normal")
-            {
-                Instantiate(bullet, transform.position +
-                new Vector3(bulletDis * Mathf.Sin(angle), 0, bulletDis * Mathf.Cos(angle)),
-                transform.rotation);
-            }
-        }
-
-
-        if (reloadTime > 0)
-        {
-            reloadTime -= Time.deltaTime;
-            reloadTime = Mathf.Max(reloadTime, 0);
-        }
-
-        if (rotating && playerRb.constraints != RigidbodyConstraints.FreezePosition)
-        {
-            playerRb.freezeRotation = false;
-            transform.Rotate(0, rotatingSpeed, 0);
-            playerRb.freezeRotation = true;
-        }
     }
 
-    IEnumerator ableToMove()
-    {
-        yield return new WaitForSeconds(0.3f);
-        playerRb.constraints = RigidbodyConstraints.None;
-        playerRb.AddRelativeForce(new Vector3(0, 0, -speed * 30), ForceMode.Force);
-    }
+    //    if (reloadTime == 0) {
+
+    //        reloadTime = 2;
+
+    //        float angle = transform.rotation.ToEulerAngles().y;
+
+    //        if (shootMode == "laser")
+    //        {
+    //            //5000 is half the length of laserbeam
+    //            Instantiate(laser, transform.position +
+    //            new Vector3((bulletDis + 5000) * Mathf.Sin(angle), 0, (bulletDis + 5000) * Mathf.Cos(angle)),
+    //            transform.rotation);
+
+    //            playerAudio.PlayOneShot(laserSound);
+
+    //            playerRb.constraints = RigidbodyConstraints.FreezePosition;
+
+    //            StartCoroutine("ableToMove");
+
+    //            shootMode = "normal";
+    //        }
+
+    //        if (shootMode == "normal")
+    //        {
+    //            Instantiate(bullet, transform.position +
+    //            new Vector3(bulletDis * Mathf.Sin(angle), 0, bulletDis * Mathf.Cos(angle)),
+    //            transform.rotation);
+    //        }
+    //    }
+
+
+    //    if (reloadTime > 0)
+    //    {
+    //        reloadTime -= Time.deltaTime;
+    //        reloadTime = Mathf.Max(reloadTime, 0);
+    //    }
+
+    //    if (rotating && playerRb.constraints != RigidbodyConstraints.FreezePosition)
+    //    {
+    //        playerRb.freezeRotation = false;
+    //        transform.Rotate(0, rotatingSpeed, 0);
+    //        playerRb.freezeRotation = true;
+    //    }
+    //}
+
+    //IEnumerator ableToMove()
+    //{
+    //    yield return new WaitForSeconds(0.3f);
+    //    playerRb.constraints = RigidbodyConstraints.None;
+    //    playerRb.AddRelativeForce(new Vector3(0, 0, -speed * 30), ForceMode.Force);
+    //}
 }
