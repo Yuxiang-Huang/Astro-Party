ffc379bb2ee57c9a535d5186d92668261c14f558
Yuxiang Huang <yhuang30@stuy.edu>
2022-08-27 21:27:46 -0400

    Suffering with an AI bot that doesn't move

---
+++ Astro Party/Assets/Yuxiang/Scripts/BotMove.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BotMove : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    int speed = 750;
+    float rotatingSpeed = 1.5f;
+    bool rotating;
+    int bulletDis = 250;
+
+    public float reloadTime;
+
+    public string shootMode;
+    //normal, laser
+
+    public AudioClip laserSound;
+
+    public GameObject laser;
+    public GameObject bullet;
+    public GameObject[] bulletAnimation;
+
+    Rigidbody playerRb;
+    AudioSource playerAudio;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
+
+        shootMode = "laser";
+
+        //Better AI move for later
+        player = GameObject.Find("Player Red");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        agent.SetDestination(player.transform.position);
+        Debug.Log(player.transform.position);
+
+        if (reloadTime == 0) {
+
+            reloadTime = 2;
+
+            float angle = transform.rotation.ToEulerAngles().y;
+
+            if (shootMode == "laser")
+            {
+                //5000 is half the length of laserbeam
+                Instantiate(laser, transform.position +
+                new Vector3((bulletDis + 5000) * Mathf.Sin(angle), 0, (bulletDis + 5000) * Mathf.Cos(angle)),
+                transform.rotation);
+
+                playerAudio.PlayOneShot(laserSound);
+
+                playerRb.constraints = RigidbodyConstraints.FreezePosition;
+
+                StartCoroutine("ableToMove");
+
+                shootMode = "normal";
+            }
+
+            if (shootMode == "normal")
+            {
+                Instantiate(bullet, transform.position +
+                new Vector3(bulletDis * Mathf.Sin(angle), 0, bulletDis * Mathf.Cos(angle)),
+                transform.rotation);
+            }
+        }
+
+
+        if (reloadTime > 0)
+        {
+            reloadTime -= Time.deltaTime;
+            reloadTime = Mathf.Max(reloadTime, 0);
+        }
+
+        if (rotating && playerRb.constraints != RigidbodyConstraints.FreezePosition)
+        {
+            playerRb.freezeRotation = false;
+            transform.Rotate(0, rotatingSpeed, 0);
+            playerRb.freezeRotation = true;
+        }
+    }
+
+    IEnumerator ableToMove()
+    {
+        yield return new WaitForSeconds(0.3f);
+        playerRb.constraints = RigidbodyConstraints.None;
+        playerRb.AddRelativeForce(new Vector3(0, 0, -speed * 30), ForceMode.Force);
+    }
+}
---
+++ Astro Party/Assets/Yuxiang/Scripts/ScreenManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject startScreen;
+    public GameObject shipScreen;
+    public GameObject lastScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startScreen.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextToShip ()
+    {
+        startScreen.SetActive(false);
+        shipScreen.SetActive(true);
+    }
+
+    public void nextToLast()
+    {
+        shipScreen.SetActive(false);
+        lastScreen.SetActive(true);
+    }
+
+    public void backToStart()
+    {
+        shipScreen.SetActive(false);
+        startScreen.SetActive(true);
+    }
+
+    public void backToShip()
+    {
+        lastScreen.SetActive(false);
+        shipScreen.SetActive(true);
+    }
+
+    public void play()
+    {
+        lastScreen.SetActive(false);
+    }
+}
