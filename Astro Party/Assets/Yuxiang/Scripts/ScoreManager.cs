076d625593f5b5e8e286c8b91d00daefe3243c8c
Yuxiang Huang <yhuang30@stuy.edu>
2022-08-30 21:40:54 -0400

    It finally worked !!!

--- Astro Party/Assets/Yuxiang/Scripts/ScoreManager.cs
+++ Astro Party/Assets/Yuxiang/Scripts/ScoreManager.cs
@@ -28,7 +28,7 @@ public class ScoreManager : MonoBehaviour
     {
         gameManagerScript = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
-        lengthOfSquare = 500 / 5;
+        lengthOfSquare = 500 / 5 * 2;
     }
 
     // Update is called once per frame
@@ -42,33 +42,29 @@ public class ScoreManager : MonoBehaviour
 
         scoreScreen.SetActive(true);
 
-        yield return new WaitForSeconds(1f);
-
-        Debug.Log(P1.transform.position.x + relativeOffSet);
-        Debug.Log(startPosX + P1Score * lengthOfSquare);
-
-        if (! closeEnough(P1.transform.position.x + relativeOffSet, startPosX + P1Score * lengthOfSquare))
+        while (! closeEnough(P1.transform.position.x + relativeOffSet, startPosX + P1Score * lengthOfSquare))
         {
 
             yield return new WaitForSeconds(1f);
             P1.transform.position = new Vector3(P1.transform.position.x + lengthOfSquare, P1.transform.position.y,
                 P1.transform.position.z);
 
-            Debug.Log(P1.transform.position.x);
+            Debug.Log(P1.transform.position.x + relativeOffSet);
+            Debug.Log(startPosX + P1Score * lengthOfSquare);
         }
 
-        Debug.Log(P2.transform.position.x + relativeOffSet);
-        Debug.Log(startPosX + P2Score * lengthOfSquare);
-
-        if (! closeEnough(P2.transform.position.x + relativeOffSet, startPosX + P2Score * lengthOfSquare))
+        while (! closeEnough(P2.transform.position.x + relativeOffSet, startPosX + P2Score * lengthOfSquare))
         {
 
             yield return new WaitForSeconds(1f);
             P2.transform.position = new Vector3(P2.transform.position.x + lengthOfSquare, P2.transform.position.y,
                 P2.transform.position.z);
 
-            Debug.Log(P2.transform.position.x);
+            Debug.Log(P2.transform.position.x + relativeOffSet);
+            Debug.Log(startPosX + P2Score * lengthOfSquare);
         }
+
+        yield return new WaitForSeconds(2f);
 
         scoreScreen.SetActive(false);
         gameManagerScript.spawnShips();
@@ -76,7 +72,7 @@ public class ScoreManager : MonoBehaviour
 
     bool closeEnough(float one, float two)
     {
-        return Mathf.Abs(Mathf.Abs(two - one) / two) < 0.01;
+        return Mathf.Abs(one - two) < 1;
     }
 
     public void rematch()
