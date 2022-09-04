d62b240e3d20c3a0522da4332b400d64858387cc
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-03 23:26:27 -0400

    Code for powerUp attraction to ship done.

--- Astro Party/Assets/Yuxiang/Scripts/PowerUp.cs
+++ Astro Party/Assets/Yuxiang/Scripts/PowerUp.cs
@@ -6,15 +6,36 @@ public class PowerUp : MonoBehaviour
 {
     public string powerUpName;
 
+    GameManager gameManagerScript;
+
+    public int attractDist = 100;
+
+    public int attracSpeed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        gameManagerScript = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreach (List<GameObject> shipList in gameManagerScript.inGameShips)
+        {
+            foreach (GameObject ship in shipList)
+            {
+                if (distance(ship, this.gameObject) <= attractDist)
+                {
+                    transform.position += (transform.position - ship.transform.position).normalized * attracSpeed;
+                }
+            }
+        }
+    }
 
+    float distance(GameObject ship1, GameObject ship2)
+    {
+        return Mathf.Sqrt(Mathf.Pow((ship1.transform.position.x - ship2.transform.position.x), 2) +
+            Mathf.Pow((ship1.transform.position.z - ship2.transform.position.z), 2));
     }
 }
