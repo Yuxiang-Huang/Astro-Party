3fe6e18dec198d33db1f0179e93d576992bd67a0
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-05 21:24:44 -0400

    Asteroids script done

---
+++ Astro Party/Assets/Yuxiang/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public string type;
+
+    PowerUpManager powerUpManagerScript;
+
+    int health;
+
+    GameObject powerUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerUpManagerScript = GameObject.Find("PowerUp Manager").GetComponent<PowerUpManager>();
+
+        switch (type)
+        {
+            case "large": health = 3; break;
+
+            case "medium": health = 2; break;
+
+            case "small": health = 1; break;
+
+            case "powerUp": health = 1;
+                powerUp = powerUpManagerScript.indicators[Random.Range(0, powerUpManagerScript.indicators.Count)];
+                break;
+        }
+    }
+}
