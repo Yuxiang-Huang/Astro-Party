900f7e2a626dcfc030423853e8286b3bd64c5daf
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-05 20:53:15 -0400

    Code done for power up asteroid

--- Astro Party/Assets/Yuxiang/Scripts/PowerUpAsteroid.cs
+++ Astro Party/Assets/Yuxiang/Scripts/PowerUpAsteroid.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PowerUpAsteroid : MonoBehaviour
 {
+    PowerUpManager powerUpManagerScript;
+
+    int health;
+
+    GameObject powerUp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerUpManagerScript = GameObject.Find("PowerUp Manager").GetComponent<PowerUpManager>();
+
+        powerUp = powerUpManagerScript.indicators[Random.Range(0, powerUpManagerScript.indicators.Count)];
+
+        health = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        health--;
+        if (health == 0)
+        {
+            Destroy(this.gameObject);
+            Instantiate(powerUp, transform.position, powerUp.transform.rotation);
+        }
     }
 }
