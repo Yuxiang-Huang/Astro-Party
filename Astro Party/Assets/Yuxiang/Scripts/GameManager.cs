244ee944f96fe1f8a035bd4d8b8d2a9d31f69b5b
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-10 12:49:40 -0400

    There are 9 spawning positions now

--- Astro Party/Assets/Yuxiang/Scripts/GameManager.cs
+++ Astro Party/Assets/Yuxiang/Scripts/GameManager.cs
@@ -513,8 +513,10 @@ public class GameManager : MonoBehaviour
     void resetPosRot()
     {
         pos = new List<Vector3>() { new Vector3(spawnX, 15, spawnZ), new Vector3(-spawnX, 15, spawnZ),
-        new Vector3(-spawnX, 15, -spawnZ), new Vector3(spawnX, 15, -spawnZ), new Vector3(0, 15, 0)};
+        new Vector3(-spawnX, 15, -spawnZ), new Vector3(spawnX, 15, -spawnZ),
+            new Vector3(0, 15, -spawnZ), new Vector3(0, 15, spawnZ), new Vector3(spawnX, 15, 0), new Vector3(-spawnX, 15, 0)};
         rot = new List<Vector3>() {new Vector3(0, 180, 0), new Vector3(0, 90, 0),
-        new Vector3(0, 0, 0), new Vector3(0, -90, 0), new Vector3(0, -45, 0)};
+        new Vector3(0, 0, 0), new Vector3(0, -90, 0),
+            new Vector3(0, -45, 0), new Vector3(0, 135, 0), new Vector3(0, -135, 0), new Vector3(0, 45, 0)};
     }
 }
