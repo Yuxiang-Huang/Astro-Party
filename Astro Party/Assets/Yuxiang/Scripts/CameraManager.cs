ff73d521b3af9654e400cc4b5d27d5b399bc8b2d
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-11 12:55:30 -0400

    Just need to fix shaking now for resizing camera

--- Astro Party/Assets/Yuxiang/Scripts/CameraManager.cs
+++ Astro Party/Assets/Yuxiang/Scripts/CameraManager.cs
@@ -14,7 +14,6 @@ public class CameraManager : MonoBehaviour
         gameManagerScript = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float minX = gameManagerScript.spawnX;
@@ -26,30 +25,33 @@ public class CameraManager : MonoBehaviour
         {
             foreach (GameObject ship in shipList)
             {
-                minX = Mathf.Min(minX, ship.transform.position.x);
-                maxX = Mathf.Max(maxX, ship.transform.position.x);
-                minZ = Mathf.Min(minZ, ship.transform.position.z);
-                maxZ = Mathf.Max(maxZ, ship.transform.position.z);
+                if (ship != null)
+                {
+                    minX = Mathf.Min(minX, ship.transform.position.x);
+                    maxX = Mathf.Max(maxX, ship.transform.position.x);
+                    minZ = Mathf.Min(minZ, ship.transform.position.z);
+                    maxZ = Mathf.Max(maxZ, ship.transform.position.z);
+                }
             }
         }
 
-        //float lenX = (maxX - minX) / 2;
+        float lenX = (maxX - minX) / 2;
 
-        //float lenZ = (maxZ - minZ) / 2;
+        float lenZ = (maxZ - minZ) / 2;
 
-        //minX = Mathf.Max(minX - lenX, -gameManagerScript.spawnX);
-        //maxX = Mathf.Min(maxX + lenX, gameManagerScript.spawnX);
+        minX = Mathf.Max(minX - lenX, -gameManagerScript.spawnX);
+        maxX = Mathf.Min(maxX + lenX, gameManagerScript.spawnX);
 
-        //minZ = Mathf.Max(minZ - lenZ, -gameManagerScript.spawnZ);
-        //maxZ = Mathf.Min(maxZ + lenZ, gameManagerScript.spawnZ);
+        minZ = Mathf.Max(minZ - lenZ, -gameManagerScript.spawnZ);
+        maxZ = Mathf.Min(maxZ + lenZ, gameManagerScript.spawnZ);
 
-        myCamera.orthographicSize = Mathf.Max( (maxX - minX) / 2, (maxZ - minZ) / 2);
+        myCamera.orthographicSize = Mathf.Max(100, Mathf.Max( (maxX - minX) / 2, (maxZ - minZ) / 2));
 
         transform.position = new Vector3((minX + maxX) / 2, transform.position.y, (minZ + maxZ) / 2);
 
-        Debug.Log("minX: " + minX);
-        Debug.Log("minZ: " + minZ);
-        Debug.Log("maxX: " + maxX);
-        Debug.Log("maxZ: " +  maxZ);
+        //Debug.Log("minX: " + minX);
+        //Debug.Log("minZ: " + minZ);
+        //Debug.Log("maxX: " + maxX);
+        //Debug.Log("maxZ: " + maxZ);
     }
 }
