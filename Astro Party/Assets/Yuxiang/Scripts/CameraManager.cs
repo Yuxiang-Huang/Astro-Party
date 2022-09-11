f81e550e6c5c547db9f40baff293261500c65a52
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-11 13:01:22 -0400

    Fixed x out of boundary issue

--- Astro Party/Assets/Yuxiang/Scripts/CameraManager.cs
+++ Astro Party/Assets/Yuxiang/Scripts/CameraManager.cs
@@ -14,7 +14,7 @@ public class CameraManager : MonoBehaviour
         gameManagerScript = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
-    void Update()
+    void LateUpdate()
     {
         float minX = gameManagerScript.spawnX;
         float maxX = -gameManagerScript.spawnX;
@@ -39,17 +39,19 @@ public class CameraManager : MonoBehaviour
 
         float lenZ = (maxZ - minZ) / 2;
 
+        Debug.Log("minX before: " + minX);
+
         minX = Mathf.Max(minX - lenX, -gameManagerScript.spawnX);
         maxX = Mathf.Min(maxX + lenX, gameManagerScript.spawnX);
 
         minZ = Mathf.Max(minZ - lenZ, -gameManagerScript.spawnZ);
         maxZ = Mathf.Min(maxZ + lenZ, gameManagerScript.spawnZ);
 
-        myCamera.orthographicSize = Mathf.Max(100, Mathf.Max( (maxX - minX) / 2, (maxZ - minZ) / 2));
+        myCamera.orthographicSize = Mathf.Max(1000, Mathf.Max( (maxX - minX) / 2, (maxZ - minZ) / 2)) / 2;
 
         transform.position = new Vector3((minX + maxX) / 2, transform.position.y, (minZ + maxZ) / 2);
 
-        //Debug.Log("minX: " + minX);
+        Debug.Log("minX after: " + minX);
         //Debug.Log("minZ: " + minZ);
         //Debug.Log("maxX: " + maxX);
         //Debug.Log("maxZ: " + maxZ);
