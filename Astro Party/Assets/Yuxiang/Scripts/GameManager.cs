480999373c34ce2fd35f384235ea0031310a90c3
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-17 10:09:28 -0400

    UI for starting power Up screen roughly done

--- Astro Party/Assets/Yuxiang/Scripts/GameManager.cs
+++ Astro Party/Assets/Yuxiang/Scripts/GameManager.cs
@@ -496,10 +496,10 @@ public class GameManager : MonoBehaviour
 
     void resetPosRot()
     {
-        pos = new List<Vector3>() { new Vector3(spawnX, 15, spawnZ), new Vector3(-spawnX, 15, spawnZ),
+        pos = new List<Vector3>() { new Vector3(-spawnX, 15, spawnZ), new Vector3(spawnX, 15, spawnZ),
         new Vector3(-spawnX, 15, -spawnZ), new Vector3(spawnX, 15, -spawnZ),
             new Vector3(0, 15, -spawnZ) };
-        rot = new List<Vector3>() {new Vector3(0, 180, 0), new Vector3(0, 90, 0),
+        rot = new List<Vector3>() {new Vector3(0, 90, 0), new Vector3(0, 180, 0),
         new Vector3(0, 0, 0), new Vector3(0, -90, 0),
             new Vector3(0, -45, 0)};
     }
