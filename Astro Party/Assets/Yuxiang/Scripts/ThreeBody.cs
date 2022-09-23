bd01d617079d55a8a7e87ffb82dd1e123bda73c2
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-22 22:34:47 -0400

    Adding randomness

--- Astro Party/Assets/Yuxiang/Scripts/ThreeBody.cs
+++ Astro Party/Assets/Yuxiang/Scripts/ThreeBody.cs
@@ -8,24 +8,36 @@ public class ThreeBody : MonoBehaviour
     public GameObject otherTwo;
     Rigidbody rb;
 
+    public float G;
+
+    public int spawnRange;
+    public int randomSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        reset();
     }
 
     // Update is called once per frame
     void Update()
     { 
         rb.velocity += new Vector3 ( (otherOne.transform.position.x - transform.position.x) /
-            Mathf.Pow(distance(otherOne.transform.position, transform.position), 2), 0,
+            Mathf.Pow(distance(otherOne.transform.position, transform.position), 2) * G, 0,
             (otherOne.transform.position.z - transform.position.z) /
-            Mathf.Pow(distance(otherOne.transform.position, transform.position), 2) );
+            Mathf.Pow(distance(otherOne.transform.position, transform.position), 2) * G);
     }
 
     float distance(Vector3 ship1, Vector3 ship2)
     {
         return Mathf.Sqrt(Mathf.Pow((ship1.x - ship2.x), 2) +
             Mathf.Pow((ship1.z - ship2.z), 2));
+    }
+
+    public void reset()
+    {
+        transform.position = new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+        rb.velocity = new Vector3(Random.Range(-randomSpeed, randomSpeed), 0, Random.Range(-randomSpeed, randomSpeed));
     }
 }
