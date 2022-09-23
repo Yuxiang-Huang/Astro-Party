d7eb32eea3ab3950807c9364e0528e87dabc18fb
Yuxiang Huang <yhuang30@stuy.edu>
2022-09-22 23:00:25 -0400

    Figure out the spawn position

--- Astro Party/Assets/Yuxiang/Scripts/ThreeBody.cs
+++ Astro Party/Assets/Yuxiang/Scripts/ThreeBody.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class ThreeBody : MonoBehaviour
 {
+    public int threeBodyID;
     public GameObject otherOne;
     public GameObject otherTwo;
     Rigidbody rb;
 
     public float G;
 
-    public int spawnRange;
+    public int radius = 500;
     public int randomSpeed;
 
     // Start is called before the first frame update
@@ -37,7 +38,13 @@ public class ThreeBody : MonoBehaviour
 
     public void reset()
     {
-        transform.position = new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+        switch (threeBodyID)
+        {
+            case 1: transform.position = new Vector3(0, 0, radius); break;
+            case 2: transform.position = new Vector3(radius * Mathf.Sqrt(3) / 2, 0, -radius/2); break;
+            case 3: transform.position = new Vector3(-radius * Mathf.Sqrt(3) / 2, 0, -radius / 2); break;
+        }
+
         rb.velocity = new Vector3(Random.Range(-randomSpeed, randomSpeed), 0, Random.Range(-randomSpeed, randomSpeed));
     }
 }
