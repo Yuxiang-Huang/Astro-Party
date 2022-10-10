058bf7066daf89386301167db3df50d291d938d1
Yuxiang Huang <yhuang30@stuy.edu>
2022-10-09 22:16:34 -0400

    Able to go from one direction to the next in Tutorial

--- Astro Party/Assets/Yuxiang/Scripts/Managers/Tutorial.cs
+++ Astro Party/Assets/Yuxiang/Scripts/Managers/Tutorial.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Tutorial : MonoBehaviour
 {
     public GameObject startScreen;
     public GameObject prepScreen;
+    public GameObject directionScreen;
 
     public GameObject playerShip;
 
     public List<GameObject> ships;
     public GameObject tutorialMap;
 
+    public TextMeshProUGUI direction0;
+    public TextMeshProUGUI direction1;
+
     // Start is called before the first frame update
     void Start()
     {
         prepScreen.SetActive(false);
+        directionScreen.SetActive(false);
+
         GameObject shipPlayer = Instantiate(playerShip, new Vector3(0, 10, 0),
     playerShip.transform.rotation);
         shipPlayer.SetActive(false);
 
         ships.Add(shipPlayer);
+
+        direction0.gameObject.SetActive(false);
+        direction1.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -46,5 +57,16 @@ public class Tutorial : MonoBehaviour
         prepScreen.SetActive(false);
         tutorialMap.SetActive(true);
         ships[0].SetActive(true);
+        directionScreen.SetActive(true);
+        direction0.gameObject.SetActive(true);
+    }
+
+    public void directionSwitch()
+    {
+        if (direction0.gameObject.activeSelf)
+        {
+            direction0.gameObject.SetActive(false);
+            direction1.gameObject.SetActive(true);
+        }
     }
 }
