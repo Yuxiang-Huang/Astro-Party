51c9be070a8882674728ef0274328de27187cf5c
Yuxiang Huang <yhuang30@stuy.edu>
2022-10-11 23:45:42 -0400

    An example of portals works

---
+++ Astro Party/Assets/Yuxiang/Scripts/MapRelated/Portal.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject pair;
+
+    public int offSet = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float angle = pair.transform.rotation.ToEulerAngles().y;
+
+        collision.transform.position = pair.transform.position -
+            new Vector3 (Mathf.Cos(angle) * offSet, 0, Mathf.Sin(angle) * offSet);
+        collision.transform.rotation = pair.transform.rotation;
+    }
+}
