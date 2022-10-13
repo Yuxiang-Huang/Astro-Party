9cf73cd486a64efa0cdc6419942fb48f4dfc8c7d
Yuxiang Huang <yhuang30@stuy.edu>
2022-10-12 23:02:11 -0400

    Working for angle less than π/2

--- Astro Party/Assets/Yuxiang/Scripts/MapRelated/Portal.cs
+++ Astro Party/Assets/Yuxiang/Scripts/MapRelated/Portal.cs
@@ -8,32 +8,40 @@ public class Portal : MonoBehaviour
 
     public int offSet = 50;
 
-    public bool reverse;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        float angle = pair.transform.rotation.ToEulerAngles().y;
+        float angle = pair.transform.localRotation.ToEulerAngles().y;
+
+        while (angle < 0)
+        {
+            angle += 2 * Mathf.PI;
+        }
+
+        if (angle < Mathf.PI / 2)
+        {
+            angle = Mathf.PI / 2 - angle;
+        }
 
         collision.transform.position = pair.transform.position
 + new Vector3(Mathf.Cos(angle) * offSet, 0, Mathf.Sin(angle) * offSet);
 
         collision.transform.rotation = pair.transform.rotation;
 
-        if (reverse)
-        {
-            collision.transform.Rotate(new Vector3(0, 180, 0));
-        }
+        //if (angle > Mathf.PI)
+        //{
+        //    collision.transform.Rotate(new Vector3(0, 180, 0));
+        //}
     }
 }
