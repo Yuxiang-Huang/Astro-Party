4f7d5a56d07fb4e7bd39df33dfd09279595a52a3
Yuxiang Huang <yhuang30@stuy.edu>
2022-10-14 16:31:59 -0400

    Changed all the background pictures to my own

--- javaPicMaker/Main.java
+++ javaPicMaker/Main.java
@@ -5,7 +5,7 @@ import java.lang.*;
 
 public class Main {
   public static void main(String[] args) throws InterruptedException {
-    for (int times = 0; times < 10; times ++){
+    for (int times = 10; times < 17; times ++){
       Screen s = new Screen();
       Color c;
 
