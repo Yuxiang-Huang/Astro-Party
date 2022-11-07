0c4bae6ea8a769c7edffd3f920d5d0ae3da0fdcc
Yuxiang Huang <yhuang30@stuy.edu>
2022-11-06 20:38:22 -0500

    Organized the names of the map related scritpts

--- Astro Party/Assets/Yuxiang/Scripts/MapRelated/Map7FogManager.cs
+++ Astro Party/Assets/Yuxiang/Scripts/MapRelated/Map7FogManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Map7FogManager : MonoBehaviour
 {
+    public GameObject parent;
+
+    public GameObject fog;
+
+    public int spawnLen = 800;
+
+    public float fogyValue = 90;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        reset();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void reset()
+    {
+        Destroy(parent);
+        parent = new GameObject();
+
+        for (int i = -spawnLen; i <= spawnLen; i += 100)
+        {
+            for (int j = -spawnLen; j <= spawnLen; j += 100)
+            {
+                GameObject curr = Instantiate(fog, new Vector3(i, fogyValue, j), fog.transform.rotation);
+                curr.transform.parent = parent.transform;
+            }
+        }
     }
 }
